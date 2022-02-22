8150dc63fe0949cd5b8f03fa71371355f6b77131
erclee2000 <98067054+erclee2000@users.noreply.github.com>
2022-02-22 18:28:02 -0500

    three ways to do "whileHeld"

--- src/main/java/frc/robot/commands/FrontIntake.java
+++ src/main/java/frc/robot/commands/FrontIntake.java
@@ -3,13 +3,8 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
-
 import frc.robot.RobotContainer;
-import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.Intake;
-
-import org.opencv.core.RotatedRect;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
@@ -17,12 +12,7 @@ public class FrontIntake extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final Intake m_subsystem;
 
-<<<<<<< Updated upstream:src/main/java/frc/robot/commands/TeleopDriveForever.java
-  private double m_speed;
-  private double m_rotation;
-=======
-  private static double timesRun = 0.0;
->>>>>>> Stashed changes:src/main/java/frc/robot/commands/FrontIntake.java
+  private static double timesScheduled = 0.0;
   /**
    * Creates a new ExampleCommand.
    *
@@ -32,19 +22,14 @@ public class FrontIntake extends CommandBase {
     m_subsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
-<<<<<<< Updated upstream:src/main/java/frc/robot/commands/TeleopDriveForever.java
-    m_speed = speed;
-    m_rotation = rotation;
-  }
-=======
     }
->>>>>>> Stashed changes:src/main/java/frc/robot/commands/FrontIntake.java
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     m_subsystem.frontOff();
-    System.out.println("in frontinatke's intialize");
+    timesScheduled++;
+    System.out.println("in frontinatke's intialize " + timesScheduled);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -56,21 +41,17 @@ public class FrontIntake extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-<<<<<<< Updated upstream:src/main/java/frc/robot/commands/TeleopDriveForever.java
-  public void end(boolean interrupted) {}
-=======
   public void end(boolean interrupted) {
     m_subsystem.frontOff();
     System.out.println("in frontinatke's end");
   }
->>>>>>> Stashed changes:src/main/java/frc/robot/commands/FrontIntake.java
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    /*if(RobotContainer.getInstance().getSquareButton()){
+    if(RobotContainer.getInstance().getSquareButton()){
       return false;
-    }*/
+    }
     return true;
   }
 }
--- src/main/java/frc/robot/commands/TeleopDrive.java
+++ src/main/java/frc/robot/commands/TeleopDrive.java
@@ -5,13 +5,13 @@
 package frc.robot.commands;
 import java.util.function.DoubleSupplier;
 
-import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.Drive;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class TeleopDrive extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final DriveSubsystem m_subsystem;
+  private final Drive m_subsystem;
 
   private DoubleSupplier m_speed;
   private DoubleSupplier m_rotation;
@@ -20,7 +20,7 @@ public class TeleopDrive extends CommandBase {
    *
    * @param subsystem The subsystem used by this command.
    */
-  public TeleopDrive(DriveSubsystem subsystem, DoubleSupplier speed, DoubleSupplier rotation) {
+  public TeleopDrive(Drive subsystem, DoubleSupplier speed, DoubleSupplier rotation) {
     m_subsystem = subsystem;
     m_speed = speed;
     m_rotation = rotation;
@@ -35,13 +35,12 @@ public class TeleopDrive extends CommandBase {
   @Override
   public void execute() {
     m_subsystem.activateDifferentialDrive(-m_speed.getAsDouble(), m_rotation.getAsDouble());
-    System.out.println("executing activateDifferentialDrive " + m_speed.getAsDouble() + " " + m_rotation.getAsDouble());
+    //System.out.println("executing activateDifferentialDrive " + m_speed.getAsDouble() + " " + m_rotation.getAsDouble());
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
